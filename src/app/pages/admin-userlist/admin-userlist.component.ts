fe2dcd721a54c3c1aaa0a58c288e5f560a5489d8
minhhieu1 <minhhieubuinguyen@gmail.com>
2019-09-18 13:51:14 +0700

    add userlist component

---
+++ src/app/pages/admin-userlist/admin-userlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-userlist',
+  templateUrl: './admin-userlist.component.html',
+  styleUrls: ['./admin-userlist.component.scss']
+})
+export class AdminUserlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
